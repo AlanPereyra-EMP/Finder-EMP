ec09aa60d784ebf8089869d0e79221a73707595c
Alan Pereyra <alanpereyra33@gmail.com>
2020-12-07 20:00:09 -0300

    add headers on finder-emp.php

---
+++ finder-emp.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Finder EMP
+Plugin URI: https://empralidad.com.ar/finder-emp
+Description: Finder EMP es un plugin para WordPress que añade un minijuego de búsqueda mediante un shortcode.
+Author: Empralidad
+Author URI: https://empralidad.com.ar/
+Text Domain: finder-emp
+License: GPLv2 or later
+Version: 0.0.0
+*/
+?>
